2b288a5902e0f07d88d1b9c9a3d45d1e9f394eb8
Antonin Fischer <tonda.fischer@gmail.com>
2025-10-20 06:23:43 +0200

    Convert images to jpg.

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ const CACHE_NAME = 'leeuwenhoek-escape-v3';
 const ASSETS = [
   './', './index.html', './style.css', './manifest.webmanifest',
   './engine/engine.js', './engine/puzzles.js', './engine/editor.js', './game/scenes.json',
-  './assets/workshop.png', './assets/corridor.png', './assets/study.png', './assets/chest_room.png',
-  './assets/secret_lab.png', './assets/exit.png', './assets/key_glass.png', './assets/slip1.png',
-  './assets/slip2.png', './assets/micro_schema.png', './assets/icon-192.png', './assets/icon-512.png'
+  './assets/workshop.jpg', './assets/corridor.jpg', './assets/study.jpg', './assets/chest_room.jpg',
+  './assets/secret_lab.jpg', './assets/exit.jpg', './assets/key_glass.jpg', './assets/slip1.jpg',
+  './assets/slip2.jpg', './assets/micro_schema.jpg', './assets/icon-192.jpg', './assets/icon-512.jpg'
 ];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
